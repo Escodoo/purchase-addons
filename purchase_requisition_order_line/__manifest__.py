6b9d479e3d7be8cb87767d58bae61c610a7f19f0
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-07-27 14:25:32 -0300

    [ADD] purchase_requisition_order_line

---
+++ purchase_requisition_order_line/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Requisition Order ine",
+    "summary": """
+        Purchase Requisition Order Line""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/purchase-addons",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        "views/purchase_requisition.xml",
+        "views/purchase_order_line.xml",
+    ],
+    "demo": [],
+}
