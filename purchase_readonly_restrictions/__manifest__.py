7ef3eaf13311de2c5c91424cf4318415115dfc06
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-01-30 16:31:17 -0300

    [ADD] purchase_readonly_restrictions: add new module

---
+++ purchase_readonly_restrictions/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Readonly Restrictions",
+    "summary": """
+        Purchase ReadOnly Restrictions""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/purchase-addons",
+    "depends": ["l10n_br_purchase"],
+    "data": [
+        "views/purchase_views.xml",
+    ],
+}
