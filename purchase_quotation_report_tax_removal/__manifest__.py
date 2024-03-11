b9fd1c730de77498ac8bcb53a2d36095d6c0bf8e
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-03-11 08:55:56 -0300

    [ADD] purchase_quotation_report_tax_removal: add new module

---
+++ purchase_quotation_report_tax_removal/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Quotation Report Tax Removal",
+    "summary": """
+        Removal Of Taxes in the Purchase Quotation Report""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/purchase-addons",
+    "depends": ["purchase"],
+    "data": [
+        "report/purchase_order_templates.xml",
+    ],
+}
