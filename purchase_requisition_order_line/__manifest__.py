8450f44d71c7d48c0ba688baad4bb576615412ef
kaynnan <kaynnanx1@gmail.com>
2024-09-15 18:49:38 -0300

    [MIG] purchase_requisition_order_line: Migration to 16.0

--- purchase_requisition_order_line/__manifest__.py
+++ purchase_requisition_order_line/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Adds the requisition_id field to the purchase order lines and adds an access
         link to these lines.""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/purchase-addons",
