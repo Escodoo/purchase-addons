7e0dff9affaece15938703f169afc04f0cf8c978
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-05-18 21:01:34 -0300

    [ADD] purchase_partner_selectable_option_restrict: add new module

---
+++ purchase_partner_selectable_option_restrict/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Partner Selectable Option Restrict",
+    "summary": """
+        Purchase Partner Selectable Option Restrict""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/purchase-addons",
+    "depends": ["purchase_partner_selectable_option"],
+    "data": ["views/res_partner_view.xml"],
+}
